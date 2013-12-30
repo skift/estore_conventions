bdcfa81b442654fa29d9f4295942e6684e8e1c5f
dannguyen <dansonguyen@gmail.com>
2013-12-30 16:13:37 -0500

    more separation, accuracy

---
+++ spec/integration/legacy_usage_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'legacy usage for compatability sake' do
+
+  describe '#historical_rate_per_day' do
+    context 'one-argument call' do
+      it 'should omit current day value'
+    end
+  end
+
+
+  describe '#archived_attribute_delta_by_day' do
+    context 'one-argument call' do
+      it 'should omit current day value'
+    end
+  end
+end
